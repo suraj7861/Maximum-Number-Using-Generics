2991b3fd74ce62a9ef9501e9db700f76d9f5be32
unknown <khomanesuraj8@gmail.com>
2021-10-23 22:12:49 +0530

    Refactor: class to gerics class

--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -5,17 +5,17 @@ public class Generics {
 	public static void main (String[] args) {
 		System.out.println("welcome to Generics proogram");
 		//object creation for Integer
-		MaximumNumber maxInt = new MaximumNumber(32,23,12);
+		MaximumNumber<Integer> maxInt = new MaximumNumber<Integer>(32,23,12);
 		//method call to find max Integer
 		maxInt.findMaximum();
 		
 		//object creation for Float
-		MaximumNumber maxfloat = new MaximumNumber(32.5f,23.3f,12.6f);
+		MaximumNumber<Float> maxfloat = new MaximumNumber<Float>(32.5f,23.3f,12.6f);
 		//method call to find max float
 		maxfloat.findMaximum();
 		
 		//object creation for String
-		MaximumNumber maxString = new MaximumNumber("Truck", "Ford", "Car");
+		MaximumNumber<String> maxString = new MaximumNumber<String>("Truck", "Ford", "Car");
 		//method call to find max String
 		maxString.findMaximum();
 

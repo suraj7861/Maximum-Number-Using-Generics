09aede6707095e8f9fb72a473091fb13c4e66da7
unknown <khomanesuraj8@gmail.com>
2021-10-23 20:23:26 +0530

    Added more than 3 parameter

--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -5,17 +5,17 @@ public class Generics {
 	public static void main (String[] args) {
 		System.out.println("welcome to Generics proogram");
 		//object creation for Integer
-		MaximumNumber maxInt = new MaximumNumber(32,23,12);
+		MaximumNumber maxInt = new MaximumNumber(32,23,12,43);
 		//method call to find max Integer
 		maxInt.findMaximum();
 		
 		//object creation for Float
-		MaximumNumber maxfloat = new MaximumNumber(32.5f,23.3f,12.6f);
+		MaximumNumber maxfloat = new MaximumNumber(32.5f,23.3f,12.6f,43.5f);
 		//method call to find max float
 		maxfloat.findMaximum();
 		
 		//object creation for String
-		MaximumNumber maxString = new MaximumNumber("Truck", "Ford", "Car");
+		MaximumNumber maxString = new MaximumNumber("Truck", "Ford", "Car","Tata");
 		//method call to find max String
 		maxString.findMaximum();
 
--- src/com/generics/MaximumNumber.java
+++ src/com/generics/MaximumNumber.java
@@ -2,22 +2,26 @@ package com.generics;
 
 public class MaximumNumber<T extends Comparable> {
 	//variable declaration
-	T x, y, z;
+	T x1, x2, x3, x4;
 	
-	public MaximumNumber(T x, T y, T z) {
+	public MaximumNumber(T x1, T x2, T x3, T x4) {
 		super();
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
 	}
-	//method: find maximum integer
+	//method: find maximum 
 	public void findMaximum()  {
-		T max = x;
-		if(y.compareTo(max) > 0) {
-			max = y;
+		T max = x1;
+		if(x2.compareTo(max) > 0) {
+			max = x2;
 		}
-		else if(z.compareTo(max) > 0) {
-			max = z;
+		else if(x3.compareTo(max) > 0) {
+			max = x3;
+		}
+		else if(x4.compareTo(max) > 0) {
+			max = x4;
 		}
 		System.out.println("Maximun is: "+max);
 	}	

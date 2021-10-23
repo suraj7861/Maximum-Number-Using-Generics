baf83315616628016f5077292b4d69b308c48e13
unknown <khomanesuraj8@gmail.com>
2021-10-23 19:53:56 +0530

    Refactor: All 3 method to gerics method

--- src/com/generics/Generics.java
+++ src/com/generics/Generics.java
@@ -4,20 +4,20 @@ public class Generics {
 	
 	public static void main (String[] args) {
 		System.out.println("welcome to Generics proogram");
-		//object creation for Interger
+		//object creation for Integer
 		MaximumNumber maxInt = new MaximumNumber();
-		//method call to find max int
-		maxInt.findMaxInt(32,23,12);
+		//method call to find max Integer
+		maxInt.findMaximum(32,23,12);
 		
 		//object creation for Float
 		MaximumNumber maxfloat = new MaximumNumber();
 		//method call to find max float
-		maxfloat.findMaxFloat(32.5f,23.3f,12.6f);
+		maxfloat.findMaximum(32.5f,23.3f,12.6f);
 		
 		//object creation for String
 		MaximumNumber maxString = new MaximumNumber();
-		//method call to find max float
-		maxString.maxString("Truck", "Ford", "Sedan1");
+		//method call to find max String
+		maxString.findMaximum("Truck", "Ford", "Car");
 
 	}
 }
--- src/com/generics/MaximumNumber.java
+++ src/com/generics/MaximumNumber.java
@@ -3,38 +3,15 @@ package com.generics;
 public class MaximumNumber {
 	
 	//method: find maximum integer
-	public void findMaxInt(Integer num1, Integer num2, Integer num3) {
-		Integer max = num1;
+	public <T extends Comparable> void findMaximum(T num1, T num2, T num3)  {
+		T max = num1;
 		if(num2.compareTo(max) > 0) {
 			max = num2;
 		}
 		else if(num3.compareTo(max) > 0) {
 			max = num3;
 		}
-		System.out.println("Maximun Integer number is: "+max);
-	}
-	
-	//method: find maximum float
-	public void findMaxFloat(Float num1,Float num2,Float num3) {
-		Float max = num1;
-		if(num2.compareTo(max) > 0) {
-			max = num2;
-		}
-		else if(num3.compareTo(max) > 0) {
-			max = num3;
-		}
-		System.out.println("Maximun float number is: "+max);
-	}
-	
-	//method: find maximum string
-    public void maxString(String str1, String str2, String str3){
-        String maxString = str1;
-        if (str2.compareTo(maxString) > 0){
-            maxString = str2;
-        }
-        if (str3.compareTo(maxString) > 0){
-            maxString = str3;
-        }
-        System.out.println("Maximun String is: "+maxString);
-    }
+		System.out.println("Maximun is: "+max);
+	}	
+
 }

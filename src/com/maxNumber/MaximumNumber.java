fe098a4a350b9433a6993ea456916dd2d5d2b06f
unknown <khomanesuraj8@gmail.com>
2021-10-23 15:01:46 +0530

    print welcome message

--- src/com/maxNumber/MaximumNumber.java
+++ src/com/maxNumber/MaximumNumber.java
@@ -3,6 +3,6 @@ package com.maxNumber;
 public class MaximumNumber {
 	
 	public static void main (String[] args) {
-		System.out.println("welcome to find maximum number program");
+		System.out.println("welcome to find a maximum number program");
 	}
 }
